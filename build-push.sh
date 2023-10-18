3528cd1483d337e94a076b811f85a90b99ae3731
spicata <p.ixelandian@gmail.com>
2023-10-18 16:26:50 +0800

    that.io

--- build-push.sh
+++ build-push.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
 
 green='\033[0;32m'
-red='\033[0;31m'
-blue='\033[0;34m'
 clear='\033[0m'
 
-read -p $'\e[31m> Do you want to build? [Y/n] \e[0m' build_yes
-if [[ ${build_yes} = y ]] || [[ ${build_yes} = Y ]]
-then
-	echo -e "${blue}Okay, will build.${clear}"
-	echo -e "${green}# Building...${clear}"
-	bundle exec jekyll build --destination docs/
-else
-	echo -e "${blue}Okay, will skip.${clear}"
-fi
-if [[ ${build_yes} = y ]] || [[ ${build_yes} = Y ]]
-then
-	echo -e "${green}# Adding docs...${clear}"
-	git add docs/
-else
-	echo -e "${green}# Adding rest...${clear}"
-	git aa
-	git unadd docs/
-fi
+echo -e "${green}# Building...${clear}"
+bundle exec jekyll build --destination docs/
+echo -e "${green}# Adding...${clear}"
+git add docs/
 echo -e "${green}# Committing...${clear}"
 read -p $'\e[31m> Commit name: \e[0m' name
 git cm "${name}"
-read -p $'\e[31m> Do you want to push? [Y/n] \e[0m' push_yes
-if [[ ${push_yes} = y ]] || [[ ${push_yes} = Y ]]
-then
-	echo -e "${blue}Okay, will push.${clear}"
-	echo -e "${green}# Pushing...${clear}"
-	git push
-else
-	echo -e "${blue}Okay, will skip.${clear}"
-fi
+echo -e "${green}# Pushing...${clear}"
+git push
--- nobuild.sh
+++ nobuild.sh
@@ -7,7 +7,7 @@ echo -e "${green}# Adding...${clear}"
 git aa
 git unadd docs/
 echo -e "${green}# Committing...${clear}"
-read -p $'\e[31mCommit name: \e[0m' name
+read -p $'\e[31m> Commit name: \e[0m' name
 git cm "${name}"
 echo -e "${green}# Pushing...${clear}"
 git push
